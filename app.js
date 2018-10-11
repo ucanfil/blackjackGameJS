b5b6e2d0912a2ea60184b10b79c55a5811181d38
Burak Tilek <burak.tilek@gmail.com>
2018-10-11 11:11:07 +0100

    Render card function added

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ function pickCard(deck) {
     return deck.pop()
 }
 
+console.log(shuffledDeck);
+
 var p1card1 = pickCard(shuffledDeck)
 var p1card2 = pickCard(shuffledDeck)
 var p2card1 = pickCard(shuffledDeck)
@@ -58,53 +60,63 @@ var p1card1Content = document.querySelector('.p1card1')
 var p1card2Content = document.querySelector('.p1card2')
 var p2card1Content = document.querySelector('.p2card1')
 var p2card2Content = document.querySelector('.p2card2')
+var p1scoreContent = document.querySelector('.p1score')
+var p2scoreContent = document.querySelector('.p2score')
+var winnerContent = document.querySelector('.winner')
+
+/////////////////////////
+
+p1scoreContent.textContent = p1score
+p2scoreContent.textContent = p2score
+winnerContent.textContent = winner(p1score, p2score)
+
+
+function renderCard(cardContent, card) {
+    cardContent.textContent = card[0]
+    if (card[1] == 11) {
+        cardContent.insertAdjacentHTML('afterend', ' <button class="switchValues">Switch Values</button>')
+    }
+}
+
+renderCard(p1card1Content, p1card1)
+renderCard(p1card2Content, p1card2)
+renderCard(p2card1Content, p2card1)
+renderCard(p2card2Content, p2card2)
 
 function winner(p1score, p2score) {
     var winner = null
     switch (true) {
-        case (p1score > 21 && p2score < 21):
-            winner = 'P1 Score: ' + p1score + ' P2 Score: ' + p2score + ' Player 1 Busted!'
+        case (p1score > 21):
+            winner = 'P1 Busted'
             break
-        case (p2score > 21 && p1score < 21):
-            winner = 'P1 Score: ' + p1score + ' P2 Score: ' + p2score + ' Player 2 Busted!!'
+        case (p2score > 21):
+            winner = 'P2 Busted'
             break
         case (p1score < p2score):
-            winner = 'P1 Score: ' + p1score + ' P2 Score: ' + p2score + ' Player 2 has Won!'
+            winner = 'P2 has Won!'
             break
         case (p1score > p2score):
-            winner = 'P1 Score: ' + p1score + ' P2 Score: ' + p2score + ' Player 1 has Won!'
+            winner = 'P1 has Won!'
     }
     return winner
 }
-
-/////////////////////////
-p1card1Content.textContent = p1card1[0];
-p1card2Content.textContent = p1card2[0];
-p2card1Content.textContent = p2card1[0];
-p2card2Content.textContent = p2card2[0];
-
-document.querySelector('.score').textContent = winner(p1score, p2score)
-
 
 function drawExtra(pickCard, shuffledDeck) {
     var extra = pickCard(shuffledDeck)
     return extra
 }
 
+
+
+
 document.querySelector('.p1Extra').addEventListener('click', function() {
     console.log(p1score, p2score)
-    var extra = drawExtra(pickCard, shuffledDeck)
-    document.querySelector('.p1card3').textContent = extra[0];
-    p1score = p1score + extra[1]
+    var p1card3 = drawExtra(pickCard, shuffledDeck)
+    p1card2Content.insertAdjacentHTML
+    ('afterend', '<p>Card ' + counter + ': <span class="card p1card3">' + p1card3[0] + '</span></p>')
+    var p1card3Content = document.querySelector('.p1card3')
+    renderCard(p1card3Content, p1card3)
+    p1score = p1score + p1card3[1]
     console.log(p1score, p2score)
-    document.querySelector('.score').textContent = winner(p1score, p2score)
-})
-
-document.querySelector('.p2Extra').addEventListener('click', function() {
-    console.log(p1score, p2score)
-    var extra = drawExtra(pickCard, shuffledDeck)
-    document.querySelector('.p2card3').textContent = extra[0];
-    p2score = p2score + extra[1]
-    console.log(p1score, p2score)
-    document.querySelector('.score').textContent = winner(p1score, p2score)
+    winnerContent.textContent = winner(p1score, p2score)
 })
